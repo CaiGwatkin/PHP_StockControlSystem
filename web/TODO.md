93fb02c9aa71218164f8ac2869967396b783bec4
Cai Gwatkin <caigwatkin@gmail.com>
2017-10-16 10:56:22 +1300

    Add product table and sample data to database

--- web/TODO.md
+++ web/TODO.md
@@ -2,4 +2,5 @@
 
 1. Search page
     * Products should include the following information: Stock Keeping Unit (SKU), Name, Cost, Category, Stock Quantity.
+1. Make js override submit button on register page if input error
 1. Browse page (optional)
--- web/src/model/Model.php
+++ web/src/model/Model.php
@@ -48,7 +48,7 @@ class Model
 
             $result = $this->db->query(
                     "CREATE TABLE user (
-                        id int(8) unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+                        id INT unsigned NOT NULL UNIQUE AUTO_INCREMENT,
                         username VARCHAR(256) NOT NULL UNIQUE,
                         pwd VARCHAR(256) NOT NULL,
                         name VARCHAR(256) NOT NULL,
@@ -60,57 +60,61 @@ class Model
             }
             // Add sample data, password is hashed on combination of ID and inputted password
             $pwd1 = password_hash('1'.'admin', PASSWORD_DEFAULT);
-            $pwd2 = password_hash('2'.'bobbyBOB', PASSWORD_DEFAULT);
+            $pwd2 = password_hash('2'.'TheToolman', PASSWORD_DEFAULT);
             $pwd3 = password_hash('3'.'maryMARY', PASSWORD_DEFAULT);
             $pwd4 = password_hash('4'.'joeyJOEY', PASSWORD_DEFAULT);
             if(!$this->db->query(
                     "INSERT INTO user
                     VALUES (NULL,'admin','$pwd1','Admin'),
-                        (NULL,'bob','$pwd2','Bob'),
+                        (NULL,'TheToolman','$pwd2','Tim Taylor'),
                         (NULL,'mary','$pwd3','Mary'),
-                        (NULL,'joe','$pwd4','Joe');"
+                        (NULL,'joey','$pwd4','Joey');"
             )) {
                 error_log($this->db->error);
                 throw new MySQLDatabaseException('Failed adding sample data to table: user');
             }
         }
 
-//        $result = $this->db->query("SHOW TABLES LIKE 'transfer';");
-//        if ($result->num_rows == 0) {
-//            // table doesn't exist
-//            // create it and populate with sample data
-//
-//            $result = $this->db->query(
-//                    "CREATE TABLE transfer (
-//                        id int(8) unsigned NOT NULL UNIQUE AUTO_INCREMENT,
-//                        datetimeOf DATETIME NOT NULL,
-//                        valueOf DECIMAL(19,2) unsigned NOT NULL,
-//                        fromAccount int(8) unsigned NOT NULL,
-//                        toAccount int(8) unsigned NOT NULL,
-//                        PRIMARY KEY (id),
-//                        FOREIGN KEY (fromAccount) REFERENCES user_account(id) ON DELETE CASCADE,
-//                        FOREIGN KEY (toAccount) REFERENCES user_account(id) ON DELETE CASCADE
-//            );");
-//            if (!$result) {
-//                error_log($this->db->error);
-//                throw new MySQLDatabaseException('Failed creating table: transfer');
-//            }
-//            // Add sample data, password is hashed on combination of ID and inputted password
-//            $date1 = date("Y-m-d H:i:s");
-//            $date2 = date("Y-m-d H:i:s");
-//            $date3 = date("Y-m-d H:i:s");
-//            $date4 = date("Y-m-d H:i:s");
-//            if(!$this->db->query(
-//                    "INSERT INTO transfer
-//                    VALUES (NULL,'$date1',20.00,2,3),
-//                        (NULL,'$date2',5.00,3,4),
-//                        (NULL,'$date3',8.00,3,2),
-//                        (NULL,'$date4',2.00,4,2);"
-//            )) {
-//                error_log($this->db->error);
-//                throw new MySQLDatabaseException('Failed adding sample data to table: transfer');
-//            }
-//        }
+        $result = $this->db->query("SHOW TABLES LIKE 'product';");
+        if ($result->num_rows == 0) {
+            // table doesn't exist
+            // create it and populate with sample data
+
+            $result = $this->db->query(
+                    "CREATE TABLE product (
+                        id INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+                        sku CHAR(6) NOT NULL UNIQUE,
+                        name VARCHAR(256) NOT NULL,
+                        cost DECIMAL(19,2) UNSIGNED NOT NULL,
+                        category VARCHAR(40) NOT NULL,
+                        stock MEDIUMINT UNSIGNED NOT NULL,
+                        PRIMARY KEY (id),
+                        INDEX ix_product (category)
+            );");
+            if (!$result) {
+                error_log($this->db->error);
+                throw new MySQLDatabaseException('Failed creating table: product');
+            }
+            // Add sample data
+            if(!$this->db->query(
+                    "INSERT INTO product
+                    VALUES (NULL,'HMR000','Claw Hammer',5.95,'Hammer',21),
+                        (NULL,'HMR001','Ball Pein',8.95,'Hammer',22),
+                        (NULL,'HMR002','Club Hammer',6.95,'Hammer',13),
+                        (NULL,'SCW000','Slot Screwdriver',9.95,'Screwdriver',13),
+                        (NULL,'SCW001','Phillips Screwdriver',14.95,'Screwdriver',28),
+                        (NULL,'SCW002','Pozidriv Screwdriver',14.95,'Screwdriver',40),
+                        (NULL,'KNF000','Pocket Knife',24.95,'Knife',19),
+                        (NULL,'KNF001','Locking Knife',24.95,'Knife',8),
+                        (NULL,'KNF002','Craft Knife',12.95,'Knife',21),
+                        (NULL,'TPE000','Masking Tape',8.95,'Tape',17),
+                        (NULL,'TPE001','Duct Tape',12.95,'Tape',32),
+                        (NULL,'TPE002','Electrical Tape',8.95,'Tape',32);"
+            )) {
+                error_log($this->db->error);
+                throw new MySQLDatabaseException('Failed adding sample data to table: product');
+            }
+        }
         //----------------------------------------------------------------------------
 
     }
